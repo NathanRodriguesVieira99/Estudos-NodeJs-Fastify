d1c10dbb0a7849d49d6d4575ae26fb117e24cc3b
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-09 20:51:13 -0300

    feat: add Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import fastify from "fastify";
+import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import { Routes } from "./routes/index.ts";
+
 
 const app = fastify();
 
@@ -13,4 +16,18 @@ app.register(Routes);
 // faz o servidor rodar
 app.listen({ port: 3333 }).then(() => {
   console.log(`Server is running on port: 3333`);
+});
+
+// doc com Swagger
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Books API",
+      version: "1.0.0",
+    },
+  },
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
 });
