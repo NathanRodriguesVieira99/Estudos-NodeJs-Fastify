175e5ff369ea31374f36aaa1b065df213c871ab5
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-09 19:22:33 -0300

    feat: add get book routes for listing all books and list one book by ID

---
+++ src/http/routes/get-book.ts
@@ -0,0 +1,28 @@
+import type { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma.ts";
+import { z } from "zod";
+
+// criação da rota  que lista todos os dados com o método GET
+export async function getBook(app: FastifyInstance) {
+  app.get("/books", async (request, reply) => {
+    // método que lista tudo pelo prisma
+    const books = await prisma.book.findMany();
+    return reply.status(200).send(books);
+  });
+
+  // criação da rota que lista um dado especifico
+  app.get("/books/:bookId", async (request, reply) => {
+    //validação com o zod verificando se o dado existe
+    const getBookParam = z.object({
+      bookId: z.string().uuid(),
+    });
+    // busca o dado pelo ID
+    const { bookId } = getBookParam.parse(request.params);
+    const book = await prisma.book.findUnique({
+      where: {
+        id: bookId,
+      },
+    });
+    return reply.status(200).send(book);
+  });
+}
