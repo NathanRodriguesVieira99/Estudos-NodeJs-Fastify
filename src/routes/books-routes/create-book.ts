f78ee2e759318ded838394c91b43fee0207cb709
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-10 09:29:53 -0300

    fix: remove id from response when creating a book

--- src/routes/books-routes/create-book.ts
+++ src/routes/books-routes/create-book.ts
@@ -24,7 +24,6 @@ export async function createBook(app: FastifyInstance) {
     });
 
     return reply.status(201).send({
-      id: book.id,
       title: title,
       author: author,
       description: description,
