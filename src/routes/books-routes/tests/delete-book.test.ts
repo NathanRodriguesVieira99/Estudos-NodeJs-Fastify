714255ad18366032c99eb312376a30d6d458f24d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-10 09:52:55 -0300

    test: refactor test w supertest

--- src/routes/books-routes/tests/delete-book.test.ts
+++ src/routes/books-routes/tests/delete-book.test.ts
@@ -1,29 +1,44 @@
+import app from "../../../server.ts";
+import request from "supertest";
+
 describe("Create Book to test  delete", () => {
+  // tipa o mock do ID do book
   let bookId: string;
+
+  // espera o app rodar
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  // espera o app parar
+  afterAll(async () => {
+    await app.close();
+  });
+
   it(" POST to /books should return 201 ", async () => {
     // testa a criação do livro na API
-    const response = await fetch("http://localhost:3333/books", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: "Livro Teste Delete",
-        author: "Autor Teste Delete",
-        description: "Descrição Teste Delete",
-      }),
+    const response = await request(app.server).post(`/books`).send({
+      title: "Livro Teste",
+      author: "Autor Teste",
+      description: "Descrição Teste",
     });
 
+    expect(response.status).toBe(201);
+    expect(response.body.title).toBe("Livro Teste");
+    expect(response.body.author).toBe("Autor Teste");
+    expect(response.body.description).toBe("Descrição Teste");
+
     // verifica se foi criado na API
-    console.log(`Status: ${response.status}`);
     expect(response.status).toBe(201);
 
     // pega o ID do livro criado
-    const createdBook = (await response.json()) as { bookId: string };
+    const createdBook = response.body as { id: string };
 
-    bookId = createdBook.bookId;
+    // verifica se possui ID
+    bookId = createdBook.id;
     expect(bookId).toBeDefined();
   });
+
   it("DELETE to /books/:bookId should return 204 ", async () => {
     // testa a deleção de um livro
 
@@ -33,17 +48,14 @@ describe("Create Book to test  delete", () => {
     }
 
     // faz a requisição DELETE
-    const deleteBook = await fetch(`http://localhost:3333/books/${bookId}`, {
-      method: "DELETE",
-    });
+    const deleteBook = await request(app.server).delete(`/books/${bookId}`);
 
     // verifica se a resposta do DELETE é 204
     console.log(`Status: ${deleteBook.status}`);
     expect(deleteBook.status).toBe(204);
 
     // verifica se o livro realmente foi deletado e gera 404
-
-    const getResponse = await fetch(`http://localhost:3333/books/${bookId}`);
+    const getResponse = await request(app.server).get(`/books/${bookId}`);
 
     console.log(`Status: ${getResponse.status}`);
     expect(getResponse.status).toBe(404);
